3e156652d450b17860564e73bb1a877616a62206
Siddharth S <siddharthajith97@gmail.com>
2021-06-20 10:46:23 +0530

    Rearranged the order of app.use(cors) else app crashes

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const port = process.env.PORT || 3000
 
 const app = express()
 app.use(express.json())
-app.use(cors)
 app.use(urlRouter)
+app.use(cors)
 
 app.listen(port, () => {
     console.log("Server started on port", port)
